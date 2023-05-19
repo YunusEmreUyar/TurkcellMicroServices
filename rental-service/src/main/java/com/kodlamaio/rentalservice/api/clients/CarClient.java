5f11633d09fd54a5c6ba6530322304453aaa967d
Yunus Emre Uyar <yunusemreuyarr@outlook.com>
2023-05-19 16:52:59 +0300

    The final retry to workshop.

--- rental-service/src/main/java/com/kodlamaio/rentalservice/api/clients/CarClient.java
+++ rental-service/src/main/java/com/kodlamaio/rentalservice/api/clients/CarClient.java
@@ -2,8 +2,6 @@ package com.kodlamaio.rentalservice.api.clients;
 
 import com.kodlamaio.commonpackage.utils.dto.ClientResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -12,6 +10,5 @@ import java.util.UUID;
 @FeignClient(name = "inventory-service", fallback = CarClientFallback.class)
 public interface CarClient {
     @GetMapping(value = "/api/cars/check-car-available/{carId}")
-    @Retryable(maxAttempts = 5, backoff = @Backoff(delay = 30000))
     ClientResponse checkIfCarAvailable(@PathVariable UUID carId);
 }
--- rental-service/src/main/java/com/kodlamaio/rentalservice/api/clients/CarClientFallback.java
+++ rental-service/src/main/java/com/kodlamaio/rentalservice/api/clients/CarClientFallback.java
@@ -4,6 +4,8 @@ import com.kodlamaio.commonpackage.utils.dto.ClientResponse;
 import com.kodlamaio.commonpackage.utils.exceptions.BusinessException;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
@@ -14,11 +16,12 @@ public class CarClientFallback implements CarClient {
 
 
     @Override
-    //@Retry(name = "check-is-car-available")
+    @Retry(name = "check-is-car-available")
     public ClientResponse checkIfCarAvailable(UUID carId) {
 
         log.info("INVENTORY SERVICE IS DOWN!");
         throw new BusinessException("INVENTORY_SERVICE_IS_DOWN!");
     }
+
 
 }
